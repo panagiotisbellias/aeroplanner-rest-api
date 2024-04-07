55e629044f224ffc535cec5fa3149b0ccfefe15c
jcilacad <johnchristopherilacad27@gmail.com>
2024-04-07 16:42:47 +0800

    Initial Commit

---
+++ src/main/java/com/projects/aeroplannerrestapi/AeroplannerRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.projects.aeroplannerrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AeroplannerRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AeroplannerRestApiApplication.class, args);
+	}
+
+}
