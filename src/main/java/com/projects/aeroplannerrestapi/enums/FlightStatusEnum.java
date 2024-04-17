5345087c91a2f48dfb752fcd6c9369bd0ec54e89
jcilacad <johnchristopherilacad27@gmail.com>
2024-04-17 18:58:49 +0800

    Added flight entity. Added FlightStatusEnum enum

---
+++ src/main/java/com/projects/aeroplannerrestapi/enums/FlightStatusEnum.java
@@ -0,0 +1,12 @@
+package com.projects.aeroplannerrestapi.enums;
+
+public enum FlightStatusEnum {
+    SCHEDULED,
+    DELAYED,
+    DEPARTED,
+    IN_FLIGHT,
+    ARRIVED,
+    CANCELLED,
+    DIVERTED,
+    UNKNOWN
+}
