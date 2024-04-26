25f5eca361fb1a684524563454dcfe978de873e6
Panagiotis Bellias <belliaspan@gmail.com>
2024-04-26 10:21:36 +0300

    [#23] Add unit tests for bootstrap and config packages

--- src/test/java/com/projects/aeroplannerrestapi/bootstrap/RoleSeederTest.java
+++ src/test/java/com/projects/aeroplannerrestapi/bootstrap/RoleSeederTest.java
@@ -1,4 +1,41 @@
 package com.projects.aeroplannerrestapi.bootstrap;
 
-public class RoleSeederTest {
+import com.projects.aeroplannerrestapi.bootsrap.RoleSeeder;
+import com.projects.aeroplannerrestapi.entity.Role;
+import com.projects.aeroplannerrestapi.enums.RoleEnum;
+import com.projects.aeroplannerrestapi.repository.RoleRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.context.event.ContextRefreshedEvent;
+
+@ExtendWith(MockitoExtension.class)
+class RoleSeederTest {
+
+    @InjectMocks
+    RoleSeeder roleSeeder;
+
+    @Mock
+    RoleRepository roleRepository;
+
+    @Test
+    void testConstructor() {
+        RoleSeeder roleSeeder = new RoleSeeder(roleRepository);
+        Assertions.assertInstanceOf(RoleSeeder.class, roleSeeder);
+    }
+
+    @Test
+    void testOnApplicationEvent() {
+        ContextRefreshedEvent event = Mockito.mock(ContextRefreshedEvent.class);
+        roleSeeder.onApplicationEvent(event);
+
+        Mockito.verify(roleRepository, Mockito.times(3)).existsByName(ArgumentMatchers.any(RoleEnum.class));
+        Mockito.verify(roleRepository, Mockito.times(3)).save(ArgumentMatchers.any(Role.class));
+    }
+
 }
--- src/test/java/com/projects/aeroplannerrestapi/config/JwtAuthenticationFilterTest.java
+++ src/test/java/com/projects/aeroplannerrestapi/config/JwtAuthenticationFilterTest.java
@@ -1,4 +1,149 @@
 package com.projects.aeroplannerrestapi.config;
 
-public class JwtAuthenticationFilterTest {
+import com.projects.aeroplannerrestapi.service.JwtService;
+import com.projects.aeroplannerrestapi.service.TokenBlacklistService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+
+import java.io.IOException;
+
+@ExtendWith(MockitoExtension.class)
+class JwtAuthenticationFilterTest {
+
+    @InjectMocks
+    JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Mock
+    HandlerExceptionResolver handlerExceptionResolver;
+
+    @Mock
+    TokenBlacklistService tokenBlacklistService;
+
+    @Mock
+    UserDetailsService userDetailsService;
+
+    @Mock
+    JwtService jwtService;
+
+    @Mock
+    HttpServletRequest request;
+
+    @Mock
+    HttpServletResponse response;
+
+    @Mock
+    FilterChain filterChain;
+
+    @Mock
+    UserDetails userDetails;
+
+    @Test
+    void testConstructor() {
+        JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(handlerExceptionResolver, tokenBlacklistService, userDetailsService, jwtService);
+        Assertions.assertInstanceOf(JwtAuthenticationFilter.class, jwtAuthenticationFilter);
+    }
+
+    @Test
+    void testDoFilterInternal() throws ServletException, IOException {
+        Mockito.when(request.getHeader("Authorization")).thenReturn("Bearer header");
+        Mockito.when(jwtService.extractUsername("header")).thenReturn("user email");
+        Mockito.when(userDetailsService.loadUserByUsername("user email")).thenReturn(userDetails);
+        Mockito.when(jwtService.isTokenValid("header", userDetails)).thenReturn(true);
+
+        jwtAuthenticationFilter.doFilterInternal(request, response, filterChain);
+        verifyValidFlow();
+        Mockito.verify(userDetails).getAuthorities();
+        Mockito.verify(request).getRemoteAddr();
+        Mockito.verify(request).getSession(false);
+    }
+
+    @Test
+    void testDoFilterInternalInvalidJWT() throws ServletException, IOException {
+        Mockito.when(request.getHeader("Authorization")).thenReturn("Bearer header");
+        Mockito.when(jwtService.extractUsername("header")).thenReturn("user email");
+        Mockito.when(userDetailsService.loadUserByUsername("user email")).thenReturn(userDetails);
+        Mockito.when(jwtService.isTokenValid("header", userDetails)).thenReturn(false);
+
+        jwtAuthenticationFilter.doFilterInternal(request, response, filterChain);
+        verifyValidFlow();
+        verifyNoMoreUserDetailsRequest();
+    }
+
+    void verifyValidFlow() {
+        verifyValidFlowBeforeChecks();
+        Mockito.verify(jwtService).isTokenValid("header", userDetails);
+        Mockito.verify(userDetails, Mockito.times(0)).getUsername();
+    }
+
+    @Test
+    void testDoFilterInternalNoBearerHeader() throws ServletException, IOException {
+        Mockito.when(request.getHeader("Authorization")).thenReturn("header");
+        jwtAuthenticationFilter.doFilterInternal(request, response, filterChain);
+        Mockito.verify(request).getHeader("Authorization");
+        Mockito.verifyNoInteractions(jwtService);
+        verifyNoTokenBlacklistUserDetailServices();
+        verifyNoUserDetailsNoMoreRequest();
+    }
+
+    @Test
+    void testDoFilterInternalUserEmailNull() throws ServletException, IOException {
+        Mockito.when(request.getHeader("Authorization")).thenReturn("Bearer header");
+        jwtAuthenticationFilter.doFilterInternal(request, response, filterChain);
+        verifyRequestHeaderExtractedUsername();
+        verifyNoTokenBlacklistUserDetailServices();
+        Mockito.verifyNoMoreInteractions(jwtService);
+        Mockito.verify(userDetails, Mockito.times(0)).getUsername();
+        verifyNoMoreUserDetailsRequest();
+    }
+
+    void verifyNoMoreUserDetailsRequest() {
+        Mockito.verifyNoMoreInteractions(userDetails);
+        Mockito.verifyNoMoreInteractions(request);
+    }
+
+    void verifyNoTokenBlacklistUserDetailServices() {
+        Mockito.verifyNoInteractions(tokenBlacklistService);
+        Mockito.verifyNoInteractions(userDetailsService);
+    }
+
+    @Test
+    void testDoFilterInternalException() throws ServletException, IOException {
+        Mockito.when(request.getHeader("Authorization")).thenReturn("Bearer header");
+        Mockito.when(jwtService.extractUsername("header")).thenReturn("user email");
+        Mockito.when(userDetailsService.loadUserByUsername("user email")).thenThrow(UsernameNotFoundException.class);
+        jwtAuthenticationFilter.doFilterInternal(request, response, filterChain);
+        verifyValidFlowBeforeChecks();
+        Mockito.verifyNoMoreInteractions(jwtService);
+        verifyNoUserDetailsNoMoreRequest();
+    }
+
+    void verifyValidFlowBeforeChecks() {
+        verifyRequestHeaderExtractedUsername();
+        Mockito.verify(tokenBlacklistService).isBlacklisted("header");
+        Mockito.verify(userDetailsService).loadUserByUsername("user email");
+    }
+
+    void verifyRequestHeaderExtractedUsername() {
+        Mockito.verify(request).getHeader("Authorization");
+        Mockito.verify(jwtService).extractUsername("header");
+    }
+
+    void verifyNoUserDetailsNoMoreRequest() {
+        Mockito.verifyNoInteractions(userDetails);
+        Mockito.verifyNoMoreInteractions(request);
+    }
+
 }
