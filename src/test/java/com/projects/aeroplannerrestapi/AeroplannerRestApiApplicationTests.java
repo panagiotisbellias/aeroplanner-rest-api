1b14395720ea623ba3fbb539c7662aaaf6665176
Panagiotis Bellias <belliaspan@gmail.com>
2024-04-29 16:53:10 +0300

    [#23] Fix unit tests for main class

--- src/test/java/com/projects/aeroplannerrestapi/AeroplannerRestApiApplicationTests.java
+++ src/test/java/com/projects/aeroplannerrestapi/AeroplannerRestApiApplicationTests.java
@@ -1,13 +1,15 @@
 package com.projects.aeroplannerrestapi;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = AeroplannerRestApiApplication.class)
 class AeroplannerRestApiApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assertions.assertTrue(true);
 	}
 
 }
