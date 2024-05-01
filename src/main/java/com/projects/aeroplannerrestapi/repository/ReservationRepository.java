09836a24fb10c2418d2a4d3654ef06d62b94cfc3
jcilacad <johnchristopherilacad27@gmail.com>
2024-05-01 19:59:57 +0800

    Added auto-cancellation feature for unpaid bookings. #54

--- src/main/java/com/projects/aeroplannerrestapi/repository/ReservationRepository.java
+++ src/main/java/com/projects/aeroplannerrestapi/repository/ReservationRepository.java
@@ -1,12 +1,16 @@
 package com.projects.aeroplannerrestapi.repository;
 
 import com.projects.aeroplannerrestapi.entity.Reservation;
+import com.projects.aeroplannerrestapi.enums.ReservationStatusEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
     Optional<Reservation> findByFlightIdAndPassengerId(Long flightId, Long passengerId);
+
+    List<Reservation> findByReservationStatus (ReservationStatusEnum reservationStatusEnum);
 }
