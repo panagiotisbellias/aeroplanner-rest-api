f1210985637713a7018283885ea96a2f47d3f6f9
Panagiotis Bellias <belliaspan@gmail.com>
2024-05-01 16:12:31 +0300

    [#23] Fix unit tests for ReservationCheckTask class

--- src/test/java/com/projects/aeroplannerrestapi/scheduler/ReservationCheckTaskTest.java
+++ src/test/java/com/projects/aeroplannerrestapi/scheduler/ReservationCheckTaskTest.java
@@ -2,9 +2,12 @@ package com.projects.aeroplannerrestapi.scheduler;
 
 import com.projects.aeroplannerrestapi.entity.Flight;
 import com.projects.aeroplannerrestapi.entity.Reservation;
+import com.projects.aeroplannerrestapi.entity.Ticket;
 import com.projects.aeroplannerrestapi.enums.ReservationStatusEnum;
+import com.projects.aeroplannerrestapi.enums.TicketStatusEnum;
 import com.projects.aeroplannerrestapi.repository.FlightRepository;
 import com.projects.aeroplannerrestapi.repository.ReservationRepository;
+import com.projects.aeroplannerrestapi.repository.TicketRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -30,23 +33,31 @@ class ReservationCheckTaskTest {
     FlightRepository flightRepository;
 
     @Mock
+    TicketRepository ticketRepository;
+
+    @Mock
     Reservation reservation;
 
     @Mock
     Flight flight;
 
+    @Mock
+    Ticket ticket;
+
     @Test
     void testConstructor() {
-        ReservationCheckTask reservationCheckTask = new ReservationCheckTask(reservationRepository, flightRepository);
+        ReservationCheckTask reservationCheckTask = new ReservationCheckTask(reservationRepository, flightRepository, ticketRepository);
         Assertions.assertInstanceOf(ReservationCheckTask.class, reservationCheckTask);
     }
 
     @Test
     void testCheckReservations() {
+        Mockito.when(ticket.getTicketStatusEnum()).thenReturn(TicketStatusEnum.BOARDED);
         Mockito.when(reservation.getReservationDate()).thenReturn(LocalDateTime.now().plusDays(1).toString());
         Mockito.when(reservationRepository.findByReservationStatus(ReservationStatusEnum.CONFIRMED)).thenReturn(List.of(reservation));
         Mockito.when(reservationRepository.save(reservation)).thenReturn(reservation);
         Mockito.when(flightRepository.findById(0L)).thenReturn(Optional.of(flight));
+        Mockito.when(ticketRepository.findByReservationId("0")).thenReturn(Optional.of(ticket));
 
         reservationCheckTask.checkReservations();
         verifyCommonInvocations();
@@ -62,9 +73,22 @@ class ReservationCheckTaskTest {
     }
 
     @Test
+    void testCheckReservationsTicketIssued() {
+        Mockito.when(ticket.getTicketStatusEnum()).thenReturn(TicketStatusEnum.ISSUED);
+        Mockito.when(reservationRepository.findByReservationStatus(ReservationStatusEnum.CONFIRMED)).thenReturn(List.of(reservation));
+        Mockito.when(ticketRepository.findByReservationId("0")).thenReturn(Optional.of(ticket));
+
+        reservationCheckTask.checkReservations();
+        Mockito.verify(reservationRepository).findByReservationStatus(ReservationStatusEnum.CONFIRMED);
+        verifyInvalidDateInvocations();
+    }
+
+    @Test
     void testCheckReservationsDateNow() {
+        Mockito.when(ticket.getTicketStatusEnum()).thenReturn(TicketStatusEnum.BOARDED);
         Mockito.when(reservationRepository.findByReservationStatus(ReservationStatusEnum.CONFIRMED)).thenReturn(List.of(reservation));
         Mockito.when(reservation.getReservationDate()).thenReturn(LocalDateTime.now().toString());
+        Mockito.when(ticketRepository.findByReservationId("0")).thenReturn(Optional.of(ticket));
 
         reservationCheckTask.checkReservations();
         verifyCommonInvocations();
@@ -73,15 +97,18 @@ class ReservationCheckTaskTest {
 
     @Test
     void testCheckReservationsDateBefore() {
+        Mockito.when(ticket.getTicketStatusEnum()).thenReturn(TicketStatusEnum.BOARDED);
         Mockito.when(reservationRepository.findByReservationStatus(ReservationStatusEnum.CONFIRMED)).thenReturn(List.of(reservation));
         Mockito.when(reservation.getReservationDate()).thenReturn(LocalDateTime.now().plusDays(3).toString());
+        Mockito.when(ticketRepository.findByReservationId("0")).thenReturn(Optional.of(ticket));
 
         reservationCheckTask.checkReservations();
+        verifyCommonInvocations();
         verifyInvalidDateInvocations();
     }
 
     void verifyInvalidDateInvocations() {
-        verifyCommonInvocations();
+        Mockito.verify(reservation).getId();
         Mockito.verifyNoMoreInteractions(reservation);
         Mockito.verifyNoMoreInteractions(reservationRepository);
         Mockito.verifyNoInteractions(flightRepository);
