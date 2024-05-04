b47bf85a7f48ee50a6dc065c02c372f4c786899f
jcilacad <johnchristopherilacad27@gmail.com>
2024-05-04 19:12:55 +0800

    Added summary and api response description for swagger.

--- src/main/java/com/projects/aeroplannerrestapi/controller/PassengerController.java
+++ src/main/java/com/projects/aeroplannerrestapi/controller/PassengerController.java
@@ -3,6 +3,9 @@ package com.projects.aeroplannerrestapi.controller;
 import com.projects.aeroplannerrestapi.dto.response.PaginatedAndSortedPassengerResponse;
 import com.projects.aeroplannerrestapi.dto.response.UserResponse;
 import com.projects.aeroplannerrestapi.service.PassengerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -21,6 +24,8 @@ public class PassengerController {
     private final PassengerService adminService;
 
     @GetMapping
+    @Operation(summary = "Get passengers")
+    @ApiResponses(@ApiResponse(responseCode = "200", description = "Found the passengers"))
     public ResponseEntity<PaginatedAndSortedPassengerResponse> getPassengers(
             @RequestParam(name = "pageNum", defaultValue = "1", required = false) int pageNumber,
             @RequestParam(name = "pageSize", defaultValue = "10", required = false) int pageSize,
@@ -30,11 +35,15 @@ public class PassengerController {
     }
 
     @GetMapping(ID)
+    @Operation(summary = "Get passenger")
+    @ApiResponses(@ApiResponse(responseCode = "200", description = "Found the passenger"))
     public ResponseEntity<UserResponse> getPassenger(@PathVariable Long id) {
         return ResponseEntity.ok(adminService.getPassenger(id));
     }
 
     @DeleteMapping(ID)
+    @Operation(summary = "Delete passenger")
+    @ApiResponses(@ApiResponse(responseCode = "204", description = "Passenger deleted"))
     public ResponseEntity<Void> deletePassenger(@PathVariable Long id) {
         adminService.deletePassenger(id);
         return ResponseEntity.noContent().build();
