11780c03e0b0a9e8c3c33c9605c4572b73f4cf78
jcilacad <johnchristopherilacad27@gmail.com>
2024-05-23 19:29:58 +0800

    Added integration tests for PassengerController class.

--- src/test/java/com/projects/aeroplannerrestapi/controller/PassengerControllerIT.java
+++ src/test/java/com/projects/aeroplannerrestapi/controller/PassengerControllerIT.java
@@ -1,4 +1,20 @@
 package com.projects.aeroplannerrestapi.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class PassengerControllerIT {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    
 }
