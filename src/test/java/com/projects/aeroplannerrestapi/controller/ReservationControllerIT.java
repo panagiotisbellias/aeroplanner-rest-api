5c7d44712dffc24618a2f8092c0500e6d3c83655
jcilacad <johnchristopherilacad27@gmail.com>
2024-05-25 15:21:01 +0800

    Added test for createReservation() method.

--- src/test/java/com/projects/aeroplannerrestapi/controller/ReservationControllerIT.java
+++ src/test/java/com/projects/aeroplannerrestapi/controller/ReservationControllerIT.java
@@ -1,13 +1,35 @@
 package com.projects.aeroplannerrestapi.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.projects.aeroplannerrestapi.dto.request.ReservationRequest;
+import com.projects.aeroplannerrestapi.entity.Flight;
+import com.projects.aeroplannerrestapi.enums.FlightStatusEnum;
+import com.projects.aeroplannerrestapi.enums.ReservationStatusEnum;
+import com.projects.aeroplannerrestapi.repository.FlightRepository;
 import com.projects.aeroplannerrestapi.util.AbstractContainerBaseTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.math.BigDecimal;
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
+@WithMockUser(roles = "USER")
+@ActiveProfiles("integration")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ReservationControllerIT extends AbstractContainerBaseTest {
 
@@ -16,4 +38,51 @@ public class ReservationControllerIT extends AbstractContainerBaseTest {
 
     @Autowired
     private ObjectMapper objectMapper;
+
+    @Autowired
+    FlightRepository flightRepository;
+
+    @BeforeEach
+    public void setup() {
+        flightRepository.deleteAll();
+    }
+
+    @Test
+    public void givenReservationRequest_whenCreateReservation_thenReturnReservationResponse() throws Exception {
+        // given
+        Flight flight = new Flight();
+        flight.setAirline("Airline");
+        flight.setFlightNumber("Flight Number");
+        flight.setDepartureTime("2023-04-19T15:30:00");
+        flight.setArrivalTime("2024-04-19T15:30:00");
+        flight.setDuration(Duration.between(LocalDateTime.parse("2023-04-19T15:30:00"),
+                LocalDateTime.parse("2024-04-19T15:30:00")));
+        flight.setPrice(BigDecimal.valueOf(0.0));
+        flight.setAircraftType("Aircraft Type");
+        flight.setSeatAvailability(0);
+        flight.setCurrentAvailableSeat(0);
+        flight.setStatus(FlightStatusEnum.UNKNOWN);
+        Flight savedFlight = flightRepository.save(flight);
+
+        ReservationRequest reservationRequest = new ReservationRequest();
+        reservationRequest.setPassengerId(1L);
+        reservationRequest.setFlightId(savedFlight.getId());
+        reservationRequest.setSeatNumber(1);
+        reservationRequest.setReservationDate("2024-05-19T15:30:00");
+        reservationRequest.setReservationStatus(ReservationStatusEnum.CONFIRMED);
+
+        // when
+        ResultActions resultActions = mockMvc.perform(post("/api/v1/reservations")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(reservationRequest)));
+
+        // then
+        resultActions.andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.passengerId").value(reservationRequest.getPassengerId()))
+                .andExpect(jsonPath("$.flightId").value(reservationRequest.getFlightId()))
+                .andExpect(jsonPath("$.seatNumber").value(reservationRequest.getSeatNumber()))
+                .andExpect(jsonPath("$.reservationDate").value(reservationRequest.getReservationDate()))
+                .andExpect(jsonPath("$.reservationStatus").value(reservationRequest.getReservationStatus().name()));
+    }
 }
