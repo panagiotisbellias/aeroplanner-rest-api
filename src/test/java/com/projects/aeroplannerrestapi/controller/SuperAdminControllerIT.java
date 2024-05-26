3d610544ed5fa791d8192e54d282958279e6c3bd
jcilacad <johnchristopherilacad27@gmail.com>
2024-05-26 16:04:08 +0800

    Added integration tests for SuperAdminController class.

--- src/test/java/com/projects/aeroplannerrestapi/controller/SuperAdminControllerIT.java
+++ src/test/java/com/projects/aeroplannerrestapi/controller/SuperAdminControllerIT.java
@@ -1,4 +1,19 @@
 package com.projects.aeroplannerrestapi.controller;
 
-public class SuperAdminControllerIT {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.projects.aeroplannerrestapi.util.AbstractContainerBaseTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class SuperAdminControllerIT extends AbstractContainerBaseTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 }
